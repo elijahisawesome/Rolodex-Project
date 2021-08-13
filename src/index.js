3aab83a15b16f2419a467cf63992deddd1f112e4
elijahisawesome <cheesyman4u2@yahoo.com>
2021-08-12 21:14:59 -0500

    added basic animation triggered by click. marginal cleanup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import * as THREE from 'three';
-//import testModel from './models/Rolodex.glb';
+import { AnimationMixer } from 'three';
+
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 
 
 const main = (function(){
+        let myDex;
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
         const loader = new GLTFLoader();
-
         const light = new THREE.PointLight();
-        light.position.x = 5;
-        light.position.y = 5;
-        light.position.z = 2;
-        scene.add(light);
+        const renderer = new THREE.WebGLRenderer();
+        const clock = new THREE.Clock();
+        let mixer;
 
-        let myDex;
+
+        
+
+        
         loader.load(
             '/src/models/Rolodex.glb',
             (gltf)=>{
-                console.log(gltf);
                 scene.add(gltf.scene);
-                myDex = gltf.scene;
+                myDex = gltf;
+                onLoad(gltf);
             },
             function(xhr){
                 console.log((xhr.loaded/xhr.total*100)+'% loaded');
@@ -31,30 +34,39 @@ const main = (function(){
                 console.error('An error happened');
             }
         )
-
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight, false);
-        document.body.appendChild(renderer.domElement);
-
-        /*
-        const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial({color:0x00ff00});
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-            */
-        camera.position.z = 5;
-
         function animate(){
             requestAnimationFrame(animate);
-            /*
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-            */
            if(myDex){
-                myDex.rotation.y +=0.01;
+                mixer.update(clock.getDelta());
            }
             renderer.render(scene,camera);
         }
-        animate();
+        
+        function init(){
+            light.position.x = 5;
+            light.position.y = 5;
+            light.position.z = 2;
+            scene.add(light);
+            camera.position.z = 5;
+            renderer.setSize(window.innerWidth, window.innerHeight, false);
+            document.body.appendChild(renderer.domElement);
+            animate();
+        }  
+        const onLoad = function (objects){
+            objects.scene.rotation.y = 4.71239;
+            mixer = new AnimationMixer(myDex.scene);
+            const clips = objects.animations;
+            
+
+            window.addEventListener('click', ()=>{
+                const action = mixer.clipAction(myDex.animations[0]);
+                console.log(action);
+                action.play();
+            })
+        }
+
+        
+
+        init();
 
     })()
