0240a22c2665779a828cd38cd0f2692f7ffe5471
elijahisawesome <cheesyman4u2@yahoo.com>
2021-08-22 12:53:14 -0500

    implemented per-object intersection detection

--- src/scripts/mouseEventListeners.js
+++ src/scripts/mouseEventListeners.js
@@ -7,13 +7,7 @@ const rayCaster = new THREE.Raycaster();
 const mouseHandler = function(renderer, scene, myDex, buttons, camera, mixer){
     renderer.domElement.addEventListener('click', initialOnClick, false);
     function initialOnClick(event){
-        event.preventDefault();
-        mouse.x = (event.clientX / window.innerWidth) *2-1;
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-        rayCaster.setFromCamera(mouse,camera);
-
-        let intersects = rayCaster.intersectObjects(scene.children,true);
+        let intersects = castCalc(event, scene.children);
 
         if (intersects.length > 0){
             const action = mixer.clipAction(myDex.animations[0]);
@@ -32,9 +26,26 @@ const mouseHandler = function(renderer, scene, myDex, buttons, camera, mixer){
         
     }
     function listenForButtonClicks(event){
-        
+        event.preventDefault();
+        let intersects = castCalc(event, scene.children)
+        //throw into a loop, iterate with appropriate logic.1
+        if(intersects.length > 0){
+            console.log(intersects[0].object);
+        }
         
     }
+
+
+    const castCalc = function(event, target){
+        event.preventDefault();
+        mouse.x = (event.clientX / window.innerWidth) *2-1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    
+        rayCaster.setFromCamera(mouse,camera);
+    
+        return rayCaster.intersectObjects(target,true);
+    }
 }
+
 
 export default mouseHandler;
