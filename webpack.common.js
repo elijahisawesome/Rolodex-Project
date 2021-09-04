6935f5594aa8930b167ea497c0f4fa4a681629b8
elijahisawesome <cheesyman4u2@yahoo.com>
2021-09-04 16:20:02 -0500

    pathing update

--- webpack.common.js
+++ webpack.common.js
@@ -46,6 +46,10 @@ module.exports = {
           use: [
             {
               loader: 'file-loader',
+              options:
+              {
+                  outputPath: '/'
+              }
             },
           ],
         },
