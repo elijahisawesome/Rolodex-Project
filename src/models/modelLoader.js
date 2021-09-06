a3c7deedff94258f8bd98e0367be5107edfe49fc
elijahisawesome <cheesyman4u2@yahoo.com>
2021-09-06 12:26:52 -0500

    Updates

---
+++ src/models/modelLoader.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const modelLoader = function(){
+    let models;
+    let loader = new GLTFLoader();
+    let buttonLoc = '/src/models/Default_Button.glb';
+    let dexLoc = '/src/models/Rolodex.glb';
+
+    console.log(process.env.NODE_ENV);
+
+    if (process.env.NODE_ENV == 'production'){
+        buttonLoc = '/Rolodex-Project/' + buttonLoc;
+        dexLoc = '/Rolodex-Project/' + dexLoc;
+    }
+
+    let defaultButton = loader.loadAsync(
+        buttonLoc,
+        null,
+        function(error){console.log(error)}
+    )
+    let Rolodex = loader.loadAsync(
+        dexLoc,
+        null,
+        function(error){console.log(error)});
+
+        return Promise.all([Rolodex, defaultButton]).then((results)=>{
+        models = results;
+        return models;
+    });
+
+    
+}
+
+export default modelLoader
