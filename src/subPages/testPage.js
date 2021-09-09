2855af742d32f225284800de6ec51467e37da9c4
elijahisawesome <cheesyman4u2@yahoo.com>
2021-09-08 19:33:31 -0500

    Updates

--- src/subPages/testPage.js
+++
@@ -1,9 +0,0 @@
-const test = function() { 
-    const testDiv = document.createElement('div');
-    testDiv.innerText = "ROLODEX TEST!";
-    testDiv.classList.add('test');
-
-    return testDiv;
-}
-
-export default test;
